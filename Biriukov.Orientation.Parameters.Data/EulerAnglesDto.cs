8b1ccd9c9c6927ea63bba0ea43b1e510b8cf901c
biryukovvg <biryukovvg@gmail.com>
2026-03-11 23:47:17 +0400

    Удалил ненужный больше проект Data из решения

--- Biriukov.Orientation.Parameters.Data/EulerAnglesDto.cs
+++ Biriukov.Orientation.Parameters.Data/EulerAnglesDto.cs
@@ -1,16 +1,16 @@
 ﻿namespace Biriukov.Orientation.Parameters.Data
 {
-    public enum AngleUnits
-    {
-        Degrees = 0,
-        Radians = 1,
-    }
+    //public enum AngleUnits
+    //{
+    //    Degrees = 0,
+    //    Radians = 1,
+    //}
 
-    public class EulerAnglesDto(double psi, double theta, double phi, AngleUnits units)
-    {
-        public double Psi { get; } = psi;
-        public double Theta { get; } = theta;
-        public double Phi { get; } = phi;
-        public AngleUnits Units { get; } = units;
-    }
+    //public class EulerAnglesDto(double psi, double theta, double phi, AngleUnits units)
+    //{
+    //    public double Psi { get; } = psi;
+    //    public double Theta { get; } = theta;
+    //    public double Phi { get; } = phi;
+    //    public AngleUnits Units { get; } = units;
+    //}
 }
--- Biriukov.Orientation.Parameters.Data/QuaternionDto.cs
+++ Biriukov.Orientation.Parameters.Data/QuaternionDto.cs
@@ -1,11 +1,11 @@
 ﻿namespace Biriukov.Orientation.Parameters
 {
-    public class QuaternionDto(double w, double x, double y, double z)
-    {
-        public double W { get; } = w;
-        public double X { get; } = x;
-        public double Y { get; } = y;
-        public double Z { get; } = z;
+    //public class QuaternionDto(double w, double x, double y, double z)
+    //{
+    //    public double W { get; } = w;
+    //    public double X { get; } = x;
+    //    public double Y { get; } = y;
+    //    public double Z { get; } = z;
 
-    }
+    //}
 }
